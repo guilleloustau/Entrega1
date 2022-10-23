6eff94ee81f27bedaa6a448983edf63f1ffd5167
guille.loustau <guillermoloustau@gmail.com>
2022-10-23 19:53:10 -0300

    primer entrega

---
+++ miarchivo.js
@@ -0,0 +1,39 @@
+
+for (let x = 1; x < 85; x++) {
+  console.log("Cuota numero " + x + " " + " de 45830"); 
+   
+  }
+alert ("¿Queres conocer el NUEVO CITROEN C3? ");
+
+  let nombreUs = prompt ("Ingresar su Nombre");
+
+    if(nombreUs == "") {
+
+      alert ("No Ingreso su Nombre");
+    } 
+  let apellidoUs = prompt ("Ingresar su Apellido");
+     if(apellidoUs == "") {
+       alert ("No Ingreso su Apellido");
+      } else {
+          alert ( nombreUs + " " + apellidoUs + " " +", estas mas cerca de conocer tu NUEVO CITROEN C3");
+        } 
+
+       alert ("Vas a poder financiarlo hasta en 84 cuotas")
+
+    alert ("El precio del NUEVO CITROEN C3 es de $3.850.000")
+    alert ("Solo tenes que entregar el 30% del valor del Nuevo Citroen C3, $1150000" )
+ let entrega = 1150000;
+let costo = 3850000;
+
+const resta = (costo, entrega) => costo - entrega;
+alert ("Te va a quedar un saldo por pagar en pesos de ")
+alert(resta  (3850000,1150000));
+let saldo = 2700000;
+let finan1 = 78;
+
+alert ("Que los podes pagar hasta en 78 cuotas de ")
+let resultado = (2700000/78)
+alert (resultado) 
+ 
+
+   
